7fa5860f69ac410d476a64d21441eb43fdf4ed23
Samuel Tull <st862@cam.ac.uk>
2022-03-31 09:25:42 +0100

    "adding subtract test"

--- example.py
+++ example.py
@@ -9,3 +9,6 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
     assert add(0.1,0.2) - 0.3 < 1e-6
+
+def test_subtract():
+    assert subtract(1-1) == 0
